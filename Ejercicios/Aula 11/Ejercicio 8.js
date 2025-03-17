0569e7110bf5f12339d66ed508977059246d9786
Joaquin Ismael Gonzalez <gonzalezcorporation1@gmail.com>
2025-03-17 15:17:56 +0000

    Ejercicios actualizados

---
+++ Ejercicios/Aula 11/Ejercicio 8.js
@@ -0,0 +1,4 @@
+var vel =59
+console.log(`La velocidad de su carro es ${vel} km/h`)
+if (vel > 60) {console.log(`Usted excedió el limite de velocidad, Multado!`)}
+console.log('Maneje siepre usando el cinturon de seguridad')
---
+++ Ejercicios/Aula 11/Ejercicio 9.js
@@ -0,0 +1,4 @@
+ var pais = 'Argentina'
+ console.log(`viviendo en ${pais}`)
+ if (pais == 'Argentina') {console.log('Usted es Argentino')}
+ else{console.log('Usted es Extranjero')}
