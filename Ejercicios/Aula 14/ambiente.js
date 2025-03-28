8f8eff2de7a8c5b96161e7d241d41ff5423c8e22
Joaquin Ismael Gonzalez <gonzalezcorporation1@gmail.com>
2025-03-28 14:08:47 +0000

    ejercicios

---
+++ Ejercicios/Aula 14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c<= 5) {
+    console.log(c)
+    c++
+}
+*/
+//********************************** */
+
+console.log('Va a comenzar')
+for(var c = 1; c<= 30; c++) {
+    console.log (c)
+}
+console.log('Terminó')
