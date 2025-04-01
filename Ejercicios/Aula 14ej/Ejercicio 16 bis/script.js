6baac8da5354f30361c0dbf3d75a86ea60d1870f
Joaquin Ismael Gonzalez <gonzalezcorporation1@gmail.com>
2025-04-01 12:07:44 +0100

    ejercicios js

--- Ejercicios/Aula 14ej/Ejercicio 16 bis/script.js
+++ Ejercicios/Aula 14ej/Ejercicio 16 bis/script.js
@@ -4,16 +4,34 @@ function contar() {
     let paso = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fin.value.length == 0 ||paso.value.length == 0) {
-        window.alert('[ERROR] faltan datos')
+    if (ini.value.length == 0 || fin.value.length == 0 || paso.value.length == 0) {
+        window.alert('[ERROR] Faltan datos')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>' 
         let i = Number(ini.value)
         let f = Number(fin.value)
         let p = Number(paso.value)
-        
-        for(let c = i; c<= f; c += p) {
-             res.innerHTML += `${c}`
+        if (p <= 0) {
+            window.alert('Paso inválido! Considere Paso 1')
+            p = 1
         }
+        if (i < f) {
+            
+            //Cuenta cresciente
+
+            for(let c = i; c <= f; c += p) {
+                 res.innerHTML += ` ${c} \u{1f449}`
+        }
+                 
+        }   else {
+               
+            //Cuenta decresciente
+
+            for(let c = i; c >= f; c -= p) {
+                    res.innerHTML += ` ${c} \u{1f449}`
+                }
+                
+        }
+         res.innerHTML += `\u{1f3c1}`
     }
 }
